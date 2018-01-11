86129b836f4738e860d12819245300d3cb1bd342
Rafał Tymiński <tyminski.rafal@gmail.com>
2018-01-11 22:09:56 +0100

    Adding static sources (images, layouts).

--- fitstudio/fitstudio/settings.py
+++ fitstudio/fitstudio/settings.py
@@ -119,3 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = (
+        os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_FINDERS = (
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_debug')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
